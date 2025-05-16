53e8d17e39bc4b26590e951eabdbf537e07f4f29
JhonUsuga <jhon.usuga1@udea.edu.co>
2025-05-15 22:44:04 -0500

    Proyecto Blanquito

---
+++ app/src/main/java/co/edu/udea/compumovil/gr07_20251/udeafood/CatalogFragment.kt
@@ -0,0 +1,17 @@
+package co.edu.udea.compumovil.gr07_20251.udeafood
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import co.edu.udea.compumovil.gr07_20251.udeafood.R
+
+class CatalogFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_catalog, container, false)
+    }
+}
