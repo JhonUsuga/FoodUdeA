44ad85ea96fc2b9047cee8db89025f5cf3a3e926
Sebastian <juans.munoz1@udea.edu.co>
2025-05-16 22:36:55 -0500

    Catalogo en proceso

--- app/src/main/java/co/edu/udea/compumovil/gr07_20251/udeafood/CatalogFragment.kt
+++ app/src/main/java/co/edu/udea/compumovil/gr07_20251/udeafood/CatalogFragment.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import co.edu.udea.compumovil.gr07_20251.udeafood.R
 
 class CatalogFragment : Fragment() {
+    private lateinit var listView: ListView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_catalog, container, false)
+        val view = inflater.inflate(R.layout.fragment_catalog, container, false)
+        val listView = view.findViewById<ListView>(R.id.listView)
+        return view
     }
 }
